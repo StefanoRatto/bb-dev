cb3be2d424fc33c139583f2a97970d351f04c6e9
team7 <team7261737465@gmail.com>
2024-05-30 16:27:00 -0500

    Implemented the call to subfinder.

--- workflow1.sh
+++ workflow1.sh
@@ -10,7 +10,7 @@ timestamp=$($home/now.sh)
 
 # output directory is created
 output_folder=$home/output/$timestamp
-#mkdir $output_folder
+mkdir $output_folder
 
 # loop over programs/scopes
 # all programs scope files with name starting with "urls" are processed
@@ -25,15 +25,28 @@ for file in "$input_folder"/*; do
 
     if [ "$ext" == "txt" ]; then
       if [[ "$filename" == urls* ]]; then
-        #echo "GO!!:     $filename"
         #go and be awesome...
+        #echo "GO!!:     $filename"
+        
+        # subfinder
+        #subfinder -dL $input_folder/$filename -silent -o $output_folder/subfinder_$filename
+        subfinder -dL $input_folder/$filename -silent > $output_folder/subfinder_$filename
+
+        # httpx
+        #doawesomehttpxstuff
+
+        # nuclei
+        #doawesomenucleistuff
+
       elif [[ "$filename" == _urls* ]]; then
         # skips "_urls" input files 
         #echo "SKIPPED:  $filename"
+        sleep 1
       fi
     else
       # ignores non-text files
       #echo "IGNORED:  $filename"
+      sleep 1
     fi
   fi
 done
