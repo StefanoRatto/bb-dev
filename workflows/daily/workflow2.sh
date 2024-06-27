d3f599793c76f8638a6ad203c2a20d5d476650ec
team7 <team7261737465@gmail.com>
2024-06-26 21:20:51 -0500

    ALmost completed workflow2.

--- workflows/daily/workflow2.sh
+++ workflows/daily/workflow2.sh
@@ -53,12 +53,22 @@ for file in "$input_folder"/*; do
           >> $output_folder/subfinder_$filename
 
         # nmap
-        # reads the input file from subfinder, then runs nmap on each host and appends the results in the output file
+        # reads the input file from subfinder, then runs nmap on each host 
+        # and appends the results in the output file
         while IFS= read -r line; do
+
+          # first a quick scan on all ports to see which ones are open
+          open_ports=$(nmap -T5 -p- --min-rate=10000 --open -oG - $line | \
+            grep -oP '\d+/open' | cut -d'/' -f1 | tr '\n' ',' | sed 's/,$//')
+          # then a vuln scan only on the open ports
+          nmap -sV --script vulners,vuln -p $open_ports --script-args mincvss=8.0 - $line \
+            2> /dev/null >> $output_folder/temp_$filename
           
-          # best nmap command line options
-          nmap -sV --script vulners,vuln -p- --script-args mincvss=8.0 - $line 2> /dev/null > $output_folder/nmap_$filename
-          
+          # formatting the nmap putput file pre-pending each line with the target host
+          sed "s/^/$line /" $output_folder/temp_$filename >> $output_folder/nmap_$filename
+          rm $output_folder/temp_$filename
+
+          # cvssv3 score lookup via nist nvd api v2.0 code snippet
           # CVE_ID="CVE-2021-44228"
           # echo $(echo "$(curl -s -H "apiKey: $NIST_NVD_API_KEY" "https://services.nvd.nist.gov/rest/json/cves/2.0?cveId=$CVE_ID")" | jq -r '.vulnerabilities[0].cve.metrics.cvssMetricV31[0].cvssData.baseScore')
           # echo $(curl -s "https://services.nvd.nist.gov/rest/json/cve/2.0/$cve_id?apiKey=$nvd_api_key" | jq -r '.vulnerabilities[0].cve.metrics.cvssMetricV31[0].cvssData.baseScore // .vulnerabilities[0].cve.metrics.cvssMetricV30[0].cvssData.baseScore')
@@ -82,6 +92,32 @@ for file in "$input_folder"/*; do
     fi
   fi
 done
+
+# removing empty lines in the nmap output file
+sed -i '/^$/d' $output_folder/nmap_$filename
+
+# copies all CVEs to a temp file
+grep -E "CVE-" "$output_folder/nmap_$filename" > "$output_folder/temp_$filename"
+
+# removes duplicate lines
+awk '!seen[$0]++' "$output_folder/temp_$filename" > temp && mv temp "$output_folder/temp_$filename"
+
+# if an entry is not in the results file, then the entry is added to the results file 
+# and also added to the notify file, which is the file that will be sent over email
+while IFS= read -r line; do
+  if ! grep -qF "$line" "$home/outputs/workflow2/results_$filename"; then
+    echo $line >> "$home/outputs/workflow2/results_$filename"
+    echo $line >> "$output_folder/notify_$filename"
+  fi
+done < "$output_folder/temp_$filename"
+
+rm $output_folder/temp_$filename
+  
+# if there is new content to be notify over, then the email is sent
+if [ -f "$output_folder/notify_$filename" ]; then
+  sed -i 's/$/ /' $output_folder/notify_$filename
+  $home/email.sh "bb-dev - workflow2/$timestamp" "$output_folder/notify_$filename" > /dev/null 2>&1
+fi
 
 # confirmation that the script completed successfully
 echo "[$($home/now.sh)] workflow2.sh completed at: $output_folder"
